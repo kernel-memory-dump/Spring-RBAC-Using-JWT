405fa6f50fdc0371e9947d83a688d7170ff5cf6e
srdjan svircevic <srdjan.svircevic@hotmail.com>
2024-06-21 16:42:00 +0200

    Refactor package name

---
+++ src/main/java/co/decem/AlphaApplication.java
@@ -0,0 +1,15 @@
+package co.decem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.alpha.dao")
+public class AlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaApplication.class, args);
+	}
+
+}
--- src/main/java/com/alpha/model/GetOrderById.java
+++
@@ -1,5 +0,0 @@
-package com.alpha.model;
-
-public class GetOrderById {
-    
-}
